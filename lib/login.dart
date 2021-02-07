6b399ba76e18ed1facfaba8eb790c098c7ecd606
carlosdias-ctrl <diasc3120@gamil.com>
2021-02-07 20:40:12 -0300

    Feito ajustes no botoes

--- lib/login.dart
+++ lib/login.dart
@@ -11,159 +11,153 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Container(
-        child: Column(children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / 2.5,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.brown,
-                  Colors.grey,
-                ],
-              ),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(90),
-              ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.center,
-                  child: Image.asset(
-                    'assets/logo.png',
-                    height: 240,
-                  ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.brown,
+                    Colors.grey,
+                  ],
                 ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      right: 32,
-                    ),
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
-                        color: Colors.yellow,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(90),
                 ),
-              ],
-            ),
-          ),
-          Container(
-              padding: EdgeInsets.only(top: 62),
+              ),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Container(
-                    width: MediaQuery.of(context).size.width / 1.2,
-                    height: 45,
-                    padding:
-                        EdgeInsets.only(top: 5, left: 18, right: 18, bottom: 1),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.brown,
-                            blurRadius: 5,
-                          )
-                        ]),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          icon: Icon(Icons.email),
-                          hintText: 'Email'),
-                    ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / 1.2,
-                    height: 45,
-                    margin: EdgeInsets.only(top: 32),
-                    padding:
-                        EdgeInsets.only(top: 5, left: 18, right: 18, bottom: 1),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.brown,
-                            blurRadius: 5,
-                          )
-                        ]),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          icon: Icon(Icons.vpn_key),
-                          hintText: 'Senha'),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      'assets/logo.png',
+                      height: 240,
                     ),
                   ),
                   Align(
-                    alignment: Alignment.centerRight,
+                    alignment: Alignment.bottomRight,
                     child: Padding(
-                      padding: const EdgeInsets.only(top: 16, right: 32),
-                      child: Text('Recuperar Senha'),
-                    ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / 1.2,
-                    height: 40,
-                    margin: EdgeInsets.only(top: 45),
-                    padding: EdgeInsets.only(
-                        top: 4, left: 16, right: 16, bottom: 16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                      color: Colors.grey,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.brown,
-                          blurRadius: 5,
-                        )
-                      ],
-                    ),
-                    child: Text(
-                      "Entrar",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20,
+                      padding: const EdgeInsets.only(
+                        right: 32,
+                      ),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.yellow,
+                          fontSize: 18,
+                        ),
                       ),
                     ),
                   ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / 1.2,
-                    height: 40,
-                    margin: EdgeInsets.only(top: 22),
-                    padding: EdgeInsets.only(
-                        top: 4, left: 16, right: 16, bottom: 16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(50)),
-                      color: Colors.grey,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.brown,
-                          blurRadius: 5,
-                        )
-                      ],
-                    ),
-                    child: Text(
-                      "Cadastrar",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  )
                 ],
-              ))
-        ]),
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.only(top: 62),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width / 1.2,
+                      height: 45,
+                      padding: EdgeInsets.only(
+                          top: 5, left: 18, right: 18, bottom: 1),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.brown,
+                              blurRadius: 5,
+                            )
+                          ]),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            icon: Icon(Icons.email),
+                            hintText: 'Email'),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / 1.2,
+                      height: 45,
+                      margin: EdgeInsets.only(top: 32),
+                      padding: EdgeInsets.only(
+                          top: 5, left: 18, right: 18, bottom: 1),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.brown,
+                              blurRadius: 5,
+                            )
+                          ]),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            icon: Icon(Icons.vpn_key),
+                            hintText: 'Senha'),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 16, right: 32),
+                        child: Text('Recuperar Senha'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(26.0),
+                      child: ButtonTheme(
+                        minWidth: MediaQuery.of(context).size.width / 1.2,
+                        height: 45,
+                        padding: EdgeInsets.only(
+                            top: 5, left: 18, right: 18, bottom: 1),
+                        child: RaisedButton(
+                          onPressed: () => {},
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: Text(
+                            'Entrar',
+                            style:
+                                TextStyle(color: Colors.black54, fontSize: 20),
+                          ),
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(0.0),
+                      child: ButtonTheme(
+                        minWidth: MediaQuery.of(context).size.width / 1.2,
+                        height: 45,
+                        padding: EdgeInsets.only(
+                            top: 5, left: 18, right: 18, bottom: 1),
+                        child: RaisedButton(
+                          onPressed: () => {},
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: Text(
+                            'Cadastrar',
+                            style:
+                                TextStyle(color: Colors.black54, fontSize: 20),
+                          ),
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ],
+                ))
+          ],
+        ),
       ),
     );
   }
 }
-
-/* */
